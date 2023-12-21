532dd046dfbc767bef0bd0cb25ea2db75d44fdd8
ValenteMesmo <valentemesmo@gmail.com>
2023-12-21 07:52:25 -0300

    removing typer-char element

--- elements/typer-area.js
+++ elements/typer-area.js
@@ -12,10 +12,33 @@
 
             const style = document.createElement("style");
             style.textContent = `
-                #content { 
-                    background-color: red; 
-                }                
+                span {
+                    font-weight: 300;
+                    font-size: 35px;
+                }
+
+                span[state="warning"] {
+                    background-color: rgb(255, 233, 178);
+                }
+
+                span[state="error"] {
+                    background-color: rgb(189, 146, 146);
+                }
+
+                span[state="success"] {
+                    background-color: rgb(221, 221, 221);
+                }
+
+                span[state="current"] {
+                    border-bottom: 3px solid #0a6bf9;
+                    background: rgba(255,255,255,.21);
+                }
             `;
+
+            const content = document.createElement('div');
+            content.setAttribute("id", "content")
+            this.shadowRoot.appendChild(content);
+
             this.shadowRoot.appendChild(style);
 
             document.addEventListener("keydown", e => {
@@ -31,19 +54,19 @@
                     this.shadowRoot.querySelector('[state="current"]');
 
                 if (e.key == 'Enter') {
-                    if (currentChar.getAttribute('char') == '\n')
+                    if (currentChar.textContent == '↲')
                         currentChar.setAttribute('state', 'success');
                     else
                         currentChar.setAttribute('state', 'error');
                 }
-                else if (e.key == currentChar.getAttribute('char')) {
+                else if (e.key == currentChar.textContent) {
                     currentChar.setAttribute('state', 'success');
                 }
                 else {
                     currentChar.setAttribute('state', 'error');
                 }
  
-                const nextChar = this.shadowRoot.querySelector('[state="0"]');
+                const nextChar = this.shadowRoot.querySelector('[state="none"]');
                 if(nextChar){
                     nextChar.setAttribute('state', 'current');
                 }
@@ -57,14 +80,15 @@
         setText(value) {
             this.connectedCallback();
 
-            this.shadowRoot.innerHTML = '';
+            const content = this.shadowRoot.getElementById("content");
+            content.innerHTML = '';
 
             const fragment = document.createDocumentFragment();
 
             for (const c of value) {
-                const char = document.createElement('typer-char');
-                char.setAttribute('char', c);
-                char.setAttribute('state', '0');
+                const char = document.createElement('span');
+                char.textContent =  c == '\n' ? '↲' : c;
+                char.setAttribute('state', 'none');
                 fragment.appendChild(char);
 
                 if (c == '\n') {
@@ -72,8 +96,8 @@
                 }
             }
 
-            this.shadowRoot.appendChild(fragment);
-            this.shadowRoot.querySelector('[state="0"]').setAttribute('state', 'current');
+            content.appendChild(fragment);
+            content.querySelector('[state="none"]').setAttribute('state', 'current');
         }
     }
 
